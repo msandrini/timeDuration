32a87343a6ca0a4f768a33df5d29f354ee90f257
henriquefacioli <henrique.facioli@gmail.com>
2018-01-24 11:57:16 -0200

    constr recv timeDuration

--- index.test.js
+++ index.test.js
@@ -18,6 +18,10 @@ describe('TimeDuration', () => {
 			const td = new TimeDuration(102);
 			expect(td.time).toBe(102);
 		});
+		it('should accept a TimeDuration at input', () => {
+			const td = new TimeDuration(new TimeDuration(102));
+			expect(td.time).toBe(102);
+		});
 		it('should accept string as HH:MM at input', () => {
 			const td1 = new TimeDuration('1:42');
 			expect(td1.time).toBe(102);
@@ -99,37 +103,39 @@ describe('TimeDuration', () => {
 		});
 	});
 
-	describe('_normalize', () => {
-		const td = new TimeDuration();
-		it('should create a new TimeDuration', () => {
-			expect(typeof td._normalize(102)).toEqual('number');
-		});
-		it('should return given TimeDuration', () => {
-			const td2 = new TimeDuration();
-			expect(td._normalize(td2)).toBe(0);
-		});
-	});
-
 	describe('operations', () => {
-		it('sum should sum two times', () => {
-			const td = new TimeDuration('21:21');
-			td.add('01:00');
-			expect(td.toMinutes()).toBe(1341);
+		describe('sum', () => {
+			it('should change current objct', () => {
+				const td = new TimeDuration('21:21');
+				td.add('01:00');
+				expect(td.toMinutes()).toBe(1341);
+			});
+			it('should return the new value of the object as minutes', () => {
+				const td = new TimeDuration('21:21');
+				expect(td.add('01:00')).toEqual(td);
+			});
 		});
-		it('subtract should subtract two times', () => {
-			const td = new TimeDuration(102);
-			const td2 = new TimeDuration(1, 0);
-			td.subtract(td2);
-			expect(td.toMinutes()).toBe(42);
-			td.subtract(td2);
-			expect(td.toMinutes()).toBe(-18); // idrk if this is the expected value.
+		describe('subtract', () => {
+			it('should change current objct', () => {
+				const td = new TimeDuration(102);
+				const td2 = new TimeDuration(1, 0);
+				td.subtract(td2);
+				expect(td.toMinutes()).toBe(42);
+				td.subtract(td2);
+				expect(td.toMinutes()).toBe(-18); // idrk if this is the expected value.
+			});
+			it('should return the new value of the object as minutes', () => {
+				const td = new TimeDuration(102);
+				const td2 = new TimeDuration(1, 0);
+				expect(td.subtract(td2)).toEqual(td);
+			});
 		});
-		it('multiplyBy should multply timeDuration by a value', () => {
+		it('multiplyBy should change current objct', () => {
 			const td = new TimeDuration(60);
 			td.multiplyBy(3);
 			expect(td.toMinutes()).toBe(180);
 		});
-		it('divideBy should divide timeDuration by a value', () => {
+		it('divideBy should change current objct', () => {
 			const td = new TimeDuration(180);
 			td.divideBy(3);
 			expect(td.toMinutes()).toBe(60);
